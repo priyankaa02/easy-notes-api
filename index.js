cebe32871f4c1e60c07677e6281e152fc569f8b0
priyanka <priyankaa0207@gmail.com>
2019-07-27 16:10:58 +0530

    notes api's

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+const app = express();
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json())
+
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Successfully connected to the database");
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});
+
+// define a simple route
+app.get('/', (req, res) => {
+    res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
+});
+
+// listen for requests
+require('./app/routes/note.routes.js')(app);
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+});
